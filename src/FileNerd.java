28a5d86529718d6474d60c414f5cbda8cc775ca5
TiNJiM <brannorth04@aol.com>
2016-10-31 12:49:55 -0600

    Final

---
+++ src/FileNerd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+public class FileNerd {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		Scanner sf = new Scanner(new File("C:\\temp_Larry\\NerdData.txt"));
+		String text[] = new String[5];
+		int maxIndx = -1;
+		while(sf.hasNext()){
+			maxIndx++;
+			text[maxIndx] = sf.nextLine();
+		}
+		for(int i = 0; i < text.length; i++) {
+			String testLine = text[i].substring(0, 3);
+			if (testLine.equals("The")){
+				System.out.println(text[i]);
+			}
+		}
+	}
+
+}
